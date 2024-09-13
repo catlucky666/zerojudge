f9096f25112ebbf9bee55a1936f35bd10da5e305
catlucky666 <119653278+catlucky666@users.noreply.github.com>
2024-09-13 09:03:23 +0800

    Update m931.py

--- zerojudge/m/m931/m931.py
+++ zerojudge/m/m931/m931.py
@@ -1,9 +1,9 @@
 n = int(input())
 A = []
 for i in range(n):
-    atk , def_ = map(int , input().split())
-    b = atk ** 2 + def_ ** 2
-    A.append((b , atk , def_))
+    atk , Def = map(int , input().split())
+    b = atk ** 2 + Def ** 2
+    A.append((b , atk , Def))
 A.sort(reverse = True)
 sec = A[1]
 print(sec[1] , sec[2])
