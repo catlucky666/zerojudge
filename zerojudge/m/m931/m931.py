8baf4fcf388e3f6b13626428a5ebbe3faa6864fa
catlucky666 <119653278+catlucky666@users.noreply.github.com>
2025-01-03 09:11:35 +0800

    Delete zerojudge/m/m931 directory

--- zerojudge/m/m931/m931.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-A = []
-for i in range(n):
-    atk , def_ = map(int , input().split())
-    b = atk ** 2 + def_ ** 2
-    A.append((b , atk , def_))
-A.sort(reverse = True)
-sec = A[1]
-print(sec[1] , sec[2])
